775e9d03600a52d15269f424b2d1571dfd6e8049
praveensaip <praveensaip99@gmail.com>
2024-04-09 15:28:18 +0530

    Test

--- demoapp/urls.py
+++ demoapp/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.book_list, name='book_list'),
     path('book/<int:pk>/', views.book_detail, name='book_detail'),
-    path('book/new/', views.book_create, name='book_create'),
+    path('books/new/', views.book_create, name='book_create'),
     path('book/<int:pk>/edit/', views.book_update, name='book_update'),
     path('book/<int:pk>/delete/', views.book_delete, name='book_delete'),
 ]
